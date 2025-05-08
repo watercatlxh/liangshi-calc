2bebbd01835b58febe785407f34ad7f5865b5a86
liangshi <1142607614@qq.com>
2025-05-08 21:47:22 +0800

    BUG修复，5.7面板

--- apps/beta.js
+++ apps/beta.js
@@ -8,26 +8,26 @@ export class beta extends plugin {
       event: 'message',
       priority: -1,
       rule: [
-        // 每个角色已分开，不需要此功能或不需要的角色请将对应的注释
+        // 每个角色已分开，不需要此功能或不需要的角色请将对应的注释，非图片版请切换至team分支
         {
-          reg: '^#?((极限|极限面板)?(瓦雷莎)(极限面板|面板100000000))$',
-          fnc: 'wls'
+          reg: '^#?((极限|极限面板)?(丝|思|厮|四|司|寺|祀|似|私|s|S)(刻|可|柯|克|科|客|课|壳|咳|k|K)(刻|可|柯|克|科|客|课|壳|咳|k|K)(极限面板|面板100000000))$',
+          fnc: 'beta_a'
         },
         {
-          reg: '^#?((极限|极限面板)?(伊安珊)(极限面板|面板100000000))$',
-          fnc: 'yas'
+          reg: '^#?((极限|极限面板)?(塔利雅|塔莉娅|塔莉雅|塔利亚|塔莉亚|塔利班)(极限面板|面板100000000))$',
+          fnc: 'beta_b'
         }
       ]
     })
   }
 
-  async wls () {
-    this.e.reply(segment.image(`file:///${_path}/plugins/liangshi-calc/resources/1.jpg`))
+  async beta_a () {
+    this.e.reply(segment.image(`file:///${_path}/plugins/liangshi-calc/resources/1.jpeg`))
     return true
   }
 
-  async yas () {
-    this.e.reply(segment.image(`file:///${_path}/plugins/liangshi-calc/resources/2.jpg`))
+  async beta_b () {
+    this.e.reply(segment.image(`file:///${_path}/plugins/liangshi-calc/resources/2.jpeg`))
     return true
   }
 
